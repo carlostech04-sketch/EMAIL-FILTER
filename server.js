f110a2f38f16a6ee35adf84556dd599cdef38a98
carlostech04-sketch <carlos.tech04@gmail.com>
2026-06-15 00:33:05 +0530

    fix CORS OPTIONS handler

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ require("dotenv").config();
 const app = express();
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
   res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") return res.sendStatus(200);
+  if (req.method === "OPTIONS") return res.status(200).end();
   next();
 });
 app.use(express.json());
